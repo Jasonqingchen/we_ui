ac6bcdabf9f8751e1a867321a8b216b73eaf6e74
Jasonqingchen <lqc301626@gmail.com>
2023-05-16 13:59:32 +0800

    修改事例数据

--- js/test.js
+++ js/test.js
@@ -1,6 +1,6 @@
 new Vue({
     el: '#app',
     data: function() {
-      return { testlist:[{name:"测试1"},{name:"测试2"}] }
+      return { testlist:[{name:"测试1"},{name:"测试2"},{name:"测试3"},{name:"测试4"},{name:"测试5"}] }
     }
   })
